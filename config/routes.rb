76c6660fe5296f47ff73062661539d4da1c26b48
keigo999 <highbridgek5@gmail.com>
2019-02-21 10:09:11 +0000

    トップページ編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'application#hello'
+  get "/" => "home#top"
+  post "/selection" => "users#selection"
+  get "/users/gokuu" => "users#gokuu"
+  get "users/vegita" => "users#vegita"
+  get "users/buro" => "users#buro"
+  get "users/yamu" => "users#yamu"
+  post "/total" => "users#total"
+  get "/total" => "users#total"
 end
